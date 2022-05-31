e8fa7999dff5b2314f6477de36c2e3291e193930
Vin274 <101485986+Vin274@users.noreply.github.com>
2022-06-01 00:34:40 +1200

    added event listeners for physical keys. Updated CSS.
    Wanted to be able to use physical keys as well as the mouse. got it working but had an issue with the equals button so removed it for now.
    Changed styling in css.
    Tried to keep the html minimal.

--- first-calculator/calculator.js
+++ first-calculator/calculator.js
@@ -1,142 +1,287 @@
 let statement;
 let count = 0;
-let storedValue = 0;
+let safeCount = 0;
 let addValue = 0;
 let subValue = 0;
 let multiValue = 0;
 let divValue = 0;
+//Assigned all buttons so I can call them from JS instead of from HTML
+const btn0 = document.querySelector('#btn0');
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+const btn4 = document.querySelector('#btn4');
+const btn5 = document.querySelector('#btn5');
+const btn6 = document.querySelector('#btn6');
+const btn7 = document.querySelector('#btn7');
+const btn8 = document.querySelector('#btn8');
+const btn9 = document.querySelector('#btn9');
+const btnDeci = document.querySelector('#btnDeci');
+const btnSub = document.querySelector('#btnSub');
+const btnAdd = document.querySelector('#btnAdd');
+const btnMulti = document.querySelector('#btnMulti');
+const btnDiv = document.querySelector('#btnDiv');
+const btnEqual = document.querySelector('#btnEqual');
+const btnReset = document.querySelector('#btnReset');
+const btnDel = document.querySelector('#btnDel');
+const calculatorScreen = document.querySelector('#calculatorScreen');
+const history = document.querySelector('#historyScreen');
 
-function btn0() {
-    if (count === 0) {
-        return document.getElementById('calcScreen').textContent = count +=0;
-    } else {
-        return document.getElementById('calcScreen').textContent = count +='0';
+//Using physical keys. equals isn't in yet.
+
+window.addEventListener('keydown', (event) => {
+    let digit = event.key;
+    switch (digit) {
+        case '0':
+            if (count === 0) {
+                return calculatorScreen.textContent = count += 0;
+            }
+            {
+                return calculatorScreen.textContent = count += '0';
+            }
+        case '1':
+            if (count === 0) {
+                return calculatorScreen.textContent = count += 1;
+            }
+            {
+                return calculatorScreen.textContent = count += '1';
+            }
+        case '2':
+            if (count === 0) {
+                return calculatorScreen.textContent = count += 2;
+            }
+            {
+                return calculatorScreen.textContent = count += '2';
+            };
+        case '3':
+            if (count === 0) {
+                return calculatorScreen.textContent = count += 3;
+            }
+            {
+                return calculatorScreen.textContent = count += '3';
+            };
+        case '4':
+            if (count === 0) {
+                return calculatorScreen.textContent = count += 4;
+            }
+            {
+                return calculatorScreen.textContent = count += '4';
+            };
+        case '5':
+            if (count === 0) {
+                return calculatorScreen.textContent = count += 5;
+            }
+            {
+                return calculatorScreen.textContent = count += '5';
+            };
+        case '6':
+            if (count === 0) {
+                return calculatorScreen.textContent = count += 6;
+            }
+            {
+                return calculatorScreen.textContent = count += '6';
+            };
+        case '7':
+            if (count === 0) {
+                return calculatorScreen.textContent = count += 7;
+            }
+            {
+                return calculatorScreen.textContent = count += '7';
+            };
+        case '8':
+            if (count === 0) {
+                return calculatorScreen.textContent = count += 8;
+            }
+            {
+                return calculatorScreen.textContent = count += '8';
+            };
+        case '9':
+            if (count === 0) {
+                return calculatorScreen.textContent = count += 9;
+            }
+            {
+                return calculatorScreen.textContent = count += '9';
+            };
+        case '+':
+            calculatorScreen.textContent = '+'
+            addValue = count;
+            count = 0;
+            statement = 'addition1'
+            break;
+        case '-':
+            calculatorScreen.textContent = '-'
+            subValue = count;
+            count = 0;
+            statement = 'subtract'
+            break;
+        case '*':
+            calculatorScreen.textContent = 'x'
+            multiValue = count;
+            count = 0;
+            statement = 'multiply'
+            break;
+        case '/':
+            calculatorScreen.textContent = '/'
+            divValue = count;
+            count = 0;
+            statement = 'divide'
+            break;
+        case 'Escape':
+            count = 0;
+            calculatorScreen.textContent = count;
+            history.textContent = ''
+            break;
+        case '.':
+            calculatorScreen.textContent = count += '.';
+            break;
+        case 'Backspace':
+            let removeDigit = count.substring(0, count.length - 1);
+            calculatorScreen.textContent = removeDigit;
+            count = removeDigit;
+            break;
     }
-}
-function btn1() {
+});
+
+//Using the mouse
+
+btn0.addEventListener('click', function () {
     if (count === 0) {
-        return document.getElementById('calcScreen').textContent = count +=1;
-    } else {
-        return document.getElementById('calcScreen').textContent = count +='1';
+        return calculatorScreen.textContent = count += 0;
     }
-}
-function btn2() {
+    {
+        return calculatorScreen.textContent = count += '0';
+    }
+});
+btn1.addEventListener('click', function () {
     if (count === 0) {
-        return document.getElementById('calcScreen').textContent = count +=2;
-    } else {
-        return document.getElementById('calcScreen').textContent = count +='2';
+        return calculatorScreen.textContent = count += 1;
     }
-}
-function btn3() {
+    {
+        return calculatorScreen.textContent = count += '1';
+    }
+});
+btn2.addEventListener('click', function () {
     if (count === 0) {
-        return document.getElementById('calcScreen').textContent = count +=3;
-    } else {
-        return document.getElementById('calcScreen').textContent = count +='3';
+        return calculatorScreen.textContent = count += 2;
     }
-}
-function btn4() {
+    {
+        return calculatorScreen.textContent = count += '2';
+    }
+});
+btn3.addEventListener('click', function () {
     if (count === 0) {
-        return document.getElementById('calcScreen').textContent = count +=4;
-    } else {
-        return document.getElementById('calcScreen').textContent = count +='4';
+        return calculatorScreen.textContent = count += 3;
     }
-}
-function btn5() {
+    {
+        return calculatorScreen.textContent = count += '3';
+    }
+});
+btn4.addEventListener('click', function () {
     if (count === 0) {
-        return document.getElementById('calcScreen').textContent = count +=5;
-    } else {
-        return document.getElementById('calcScreen').textContent = count +='5';
+        return calculatorScreen.textContent = count += 4;
     }
-}
-function btn6() {
+    {
+        return calculatorScreen.textContent = count += '4';
+    }
+});
+btn5.addEventListener('click', function () {
     if (count === 0) {
-        return document.getElementById('calcScreen').textContent = count +=6;
-    } else {
-        return document.getElementById('calcScreen').textContent = count +='6';
+        return calculatorScreen.textContent = count += 5;
     }
-}
-function btn7() {
+    {
+        return calculatorScreen.textContent = count += '5';
+    }
+});
+btn6.addEventListener('click', function () {
     if (count === 0) {
-        return document.getElementById('calcScreen').textContent = count +=7;
-    } else {
-        return document.getElementById('calcScreen').textContent = count +='7';
+        return calculatorScreen.textContent = count += 6;
     }
-}
-function btn8() {
+    {
+        return calculatorScreen.textContent = count += '6';
+    }
+});
+btn7.addEventListener('click', function () {
     if (count === 0) {
-        return document.getElementById('calcScreen').textContent = count +=8;
-    } else {
-        return document.getElementById('calcScreen').textContent = count +='8';
+        return calculatorScreen.textContent = count += 7;
     }
-}
-function btn9() {
+    {
+        return calculatorScreen.textContent = count += '7';
+    }
+});
+btn8.addEventListener('click', function () {
     if (count === 0) {
-        return document.getElementById('calcScreen').textContent = count +=9;
-    } else {
-        return document.getElementById('calcScreen').textContent = count +='9';
+        return calculatorScreen.textContent = count += 8;
     }
-}
-function btnAdd() {
-    document.getElementById('calcScreen').textContent = '+'
-    document.getElementById('btnAdd');
+    {
+        return calculatorScreen.textContent = count += '8';
+    }
+});
+btn9.addEventListener('click', function () {
+    if (count === 0) {
+        return calculatorScreen.textContent = count += 9;
+    }
+    {
+        return calculatorScreen.textContent = count += '9';
+    }
+});
+//Issue with buttons. When pressed multiple times count resorts back to 0
+btnAdd.addEventListener('click', function () {
+    calculatorScreen.textContent = '+'
     addValue = count;
     count = 0;
     statement = 'addition'
-}
-function btnSub() {
-    document.getElementById('calcScreen').textContent = '-'
-    document.getElementById('btnSub');
+})
+btnSub.addEventListener('click', function () {
+    calculatorScreen.textContent = '-'
     subValue = count;
     count = 0;
     statement = 'subtract'
-}
-function btnMulti() {
-    document.getElementById('calcScreen').textContent = '*'
-    document.getElementById('btnMulti');
+})
+btnMulti.addEventListener('click', function () {
+    calculatorScreen.textContent = 'x'
     multiValue = count;
     count = 0;
     statement = 'multiply'
-}
-function btnDiv() {
-    document.getElementById('calcScreen').textContent = '/'
-    document.getElementById('btnDiv');
+})
+btnDiv.addEventListener('click', function () {
+    calculatorScreen.textContent = '/'
     divValue = count;
     count = 0;
     statement = 'divide'
-}
-//Adding the preview screen will be an issue as it will clear back to 0 0. Need to figure out a way to bypass this.
-function btnReset() {
+})
+btnReset.addEventListener('click', function () {
     count = 0;
-    document.getElementById('calcScreen').textContent = count;
-}
-function btnDeci() {
-    document.getElementById('calcScreen').textContent = count += '.';
-}
-//Issue with deleting. When whole calculation is deleted, the buttons move up over the screen.
-function btnDel() {
+    calculatorScreen.textContent = count;
+    history.textContent = ''
+})
+btnDeci.addEventListener('click', function () {
+    calculatorScreen.textContent = count += '.';
+})
+btnDel.addEventListener('click', function () {
+    //When all numbers are deleted the screen ends up blank
     let removeDigit = count.substring(0, count.length - 1);
-    document.getElementById('calcScreen').textContent = removeDigit;
+    calculatorScreen.textContent = removeDigit;
     count = removeDigit;
-}
-function btnEqual() {
-    document.getElementById('btnEqual');
-    let finalAddValue = parseFloat(addValue);
-    let finalSubValue = parseFloat(subValue);
-    let finalMultiValue = parseFloat(multiValue);
-    let finalDivValue = parseFloat(divValue);
-    let finalValue = parseFloat(count);
-    if (statement === 'subtract') {
-        document.getElementById('calcScreen').textContent = finalSubValue - finalValue;
-        document.querySelector('#historyScreen').innerHTML = `${finalSubValue} - ${finalValue}`
-    } else if (statement === 'addition') {
-        document.getElementById('calcScreen').textContent = finalAddValue + finalValue;
-        document.querySelector('#historyScreen').innerHTML = `${finalAddValue} + ${finalValue}`
-    } else if (statement === 'multiply') {
-        document.getElementById('calcScreen').textContent = finalMultiValue * finalValue;
-        document.querySelector('#historyScreen').innerHTML = `${finalMultiValue} * ${finalValue}`
-    } else if (statement === 'divide') {
-        document.getElementById('calcScreen').textContent = finalDivValue / finalValue;
-        document.querySelector('#historyScreen').innerHTML = `${finalDivValue} / ${finalValue}`
+})
+btnEqual.addEventListener('click', function () {
+    //Swapped over to switch case instead of If statements. 
+    switch (statement) {
+        case 'subtract':
+            calculatorScreen.textContent = parseFloat(subValue) - parseFloat(count);
+            history.textContent = `${parseFloat(subValue)} - ${parseFloat(count)}`;
+            break;
+        case 'addition':
+            calculatorScreen.textContent = parseFloat(addValue) + parseFloat(count);
+            history.textContent = `${parseFloat(addValue)} + ${parseFloat(count)}`;
+            break;
+        case 'multiply':
+            calculatorScreen.textContent = parseFloat(multiValue) * parseFloat(count);
+            history.textContent = `${parseFloat(multiValue)} x ${parseFloat(count)}`;
+            break;
+        case 'divide':
+            calculatorScreen.textContent = parseFloat(divValue) / parseFloat(count);
+            history.textContent = `${parseFloat(divValue)} / ${parseFloat(count)}`;
+            break;
     }
     //Deleting values for buttons so when equals is pushed after the result. The screen goes back to 0.
     addValue = 0;
@@ -144,11 +289,4 @@ function btnEqual() {
     multiValue = 0;
     divValue = 0;
     count = 0;
-}
-
-
-// let equalBtn = document.querySelector('#btnEqual');
-// equalBtn.addEventListener('click', e => {
-//         return document.querySelector('#previewScreen').innerHTML = ''
-// }) 
-    
+});
